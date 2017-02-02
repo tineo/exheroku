aca10dddc9060d9ce4248c0fa9f152c36f1e819e
tineo <cesar@tineo.mobi>
2017-02-02 18:53:05 -0500

    rewind 6

--- config/prod.exs
+++ config/prod.exs
@@ -22,7 +22,7 @@ config :exheroku, Exheroku.Endpoint,
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Configure your database
-config :hello_phoenix, HelloPhoenix.Repo,
+config :exheroku, Exheroku.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
